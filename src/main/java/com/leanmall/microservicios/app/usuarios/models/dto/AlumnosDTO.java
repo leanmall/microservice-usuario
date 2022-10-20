91bc8f829797b750fe03d8a3851bebc26d26b921
Leandro Jofre <leanmall.93@gmail.com>
2022-10-20 14:26:50 -0300

    primer commit

---
+++ src/main/java/com/leanmall/microservicios/app/usuarios/models/dto/AlumnosDTO.java
@@ -0,0 +1,68 @@
+package com.leanmall.microservicios.app.usuarios.models.dto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class AlumnosDTO {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private List<String> materias;
+    private Date createAt;
+
+    public AlumnosDTO() {
+        materias = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public List<String> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(List<String> materias) {
+        this.materias = materias;
+    }
+
+}
---
+++ src/main/java/com/leanmall/microservicios/app/usuarios/models/entity/Alumnos.java
@@ -0,0 +1,82 @@
+package com.leanmall.microservicios.app.usuarios.models.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "alumnos")
+public class Alumnos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @ElementCollection
+    private List<String> materias;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+
+    public Alumnos() {
+        materias = new ArrayList<>();
+    }
+
+    @PrePersist
+    public void prePersist(){
+        createAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public List<String> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(List<String> materias) {
+        this.materias = materias;
+    }
+}
